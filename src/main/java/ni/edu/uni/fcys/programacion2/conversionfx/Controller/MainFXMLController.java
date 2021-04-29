facfb45c05c62b8f435de6ec888da6f420a1cf48
MateyR <rodianmatey@gmail.com>
2021-04-29 10:23:03 -0600

    FXML

---
+++ src/main/java/ni/edu/uni/fcys/programacion2/conversionfx/Controller/MainFXMLController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ni.edu.uni.fcys.programacion2.conversionfx.Controller;
+
+/**
+ *
+ * @author Sistemas-07
+ */
+public class MainFXMLController {
+    
+}
---
+++ src/main/java/ni/edu/uni/fcys/programacion2/conversionfx/MainFXMLController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ni.edu.uni.fcys.programacion2.conversionfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Sistemas-07
+ */
+public class MainFXMLController implements Initializable {
+
+    @FXML
+    public Button btnTemperature;
+    @FXML
+    public Button btnCurrency;
+    @FXML
+    public VBox VBoxCenter;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    public void BtnTemperatureAction(){
+        
+    }
+    
+    @FXML
+    public void BtnCurrencyAction(){
+        
+    }
+} 
